706e319eae96fcb14f52a6814ef25b760f092fd7
PhamHoangMinhTam <ptam0791@gmail.com>
2024-08-20 21:23:30 +0700

    Add files via upload

---
+++ bai2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 18:49:49 2024
+
+@author: Student
+"""
+str_input = "i’m a cat"
+
+result_title = str_input.title()
+print(result_title)
+
+result_upper = str_input.upper()
+print(result_upper)
+
+result_swapcase = str_input[0].upper() + str_input[1:].swapcase()
+print(result_swapcase)
+
+result_capitalize = str_input.capitalize()
+print(result_capitalize)
